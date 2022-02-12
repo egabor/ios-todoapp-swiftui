cc147ee0479288df0eafd69e00930cb3bc78a2af
Eszenyi Gábor <eszenyi.gabor@icloud.com>
2022-02-12 21:37:11 +0100

    Initial Commit

---
+++ ios-todoapp-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-todoapp-swiftui
+//
+//  Created by Eszenyi Gábor on 2022. 02. 12..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-todoapp-swiftui/ios_todoapp_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_todoapp_swiftuiApp.swift
+//  ios-todoapp-swiftui
+//
+//  Created by Eszenyi Gábor on 2022. 02. 12..
+//
+
+import SwiftUI
+
+@main
+struct ios_todoapp_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
