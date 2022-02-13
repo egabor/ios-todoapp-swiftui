f94338d5bf7222c7476d2bbf58dabc5f3d84336f
Eszenyi Gábor <eszenyi.gabor@icloud.com>
2022-02-13 17:07:04 +0100

    Code cleanup.

--- ios-todoapp-swiftui/Configurations/Config.swift
+++ ios-todoapp-swiftui/Configurations/Config.swift
@@ -9,7 +9,6 @@ import Foundation
 import Logging
 
 enum ConfigKey: String {
-    case bundleId = "BUNDLE_ID"
     case baseUrl = "BASE_URL"
     case apiKey = "API_KEY"
 }
@@ -24,9 +23,6 @@ class Config {
     }()
 
     // MARK: - Plist values
-    var bundleId: String {
-        value(for: .bundleId)
-    }
 
     var baseUrl: String {
         value(for: .baseUrl)
@@ -36,21 +32,11 @@ class Config {
         value(for: .apiKey)
     }
 
-    private lazy var logger: Logger = { .init(label: bundleId) }()
-
-    init() {
-        logger.debug("\(Self.self) Initialized [\(bundleId)]")
-    }
-
     private func value(for key: ConfigKey) -> String {
         guard let value = Config.infoDictionary[key.rawValue] as? String else {
-            logger.critical("💥 [\(key.rawValue)] is not set in plist file.")
+            print("💥 [\(key.rawValue)] is not set in plist file.")
             return ""
         }
         return value
-    }
-
-    deinit {
-        logger.debug("\(Self.self) Deinitialized")
     }
 }
